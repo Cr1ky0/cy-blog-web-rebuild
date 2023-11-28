ed11a855033391426256609e00778e6b27be89ec
Creekyu <xby503094716@gmail.com>
2023-11-28 19:46:17 +0800

    delete unrelated files

--- src/pages/MainPage/TestPage/index.tsx
+++ src/pages/MainPage/TestPage/index.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
-import FadeTransition from '@/pages/MainPage/TestPage/Test';
-import Test2 from './Test2';
 
 const TestPage = () => {
   const styles = { display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100vw', height: '100vh' };
-  return (
-    <div style={styles}>
-      <Test2></Test2>
-    </div>
-  );
+  return <div style={styles}></div>;
 };
 
 export default TestPage;
